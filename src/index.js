205b69d037c3c888532e9621245593c7a86701f9
Dan Neumann <danrodneu@gmail.com>
2016-02-09 06:38:48 -0600

    Fix belt export

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as Request from './Request';
 import * as Response from './Response';
 import * as Handler from './Handler';
 import * as Middleware from './Middleware';
+import * as belt from './belt';
 
 const DEV = 'development' === process.env.NODE_ENV;
 
